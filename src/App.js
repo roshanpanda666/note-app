daa454326862140d29b5157cc8bbd46bf2a0cb03
Roshan panda <roshanpanda666@gmail.com>
2023-02-26 16:55:08 +0530

    commiting 2nd  time

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
 
   const[inputList,setList]=useState("")
   const[Items,afterClick]=useState([])
+  const[bar,leftbar]=useState("noAnimatebar")
 
   const itemEvent=(event)=>{
       setList(event.target.value)
@@ -12,11 +13,19 @@ function App() {
     afterClick((oldItems)=>{
       return[...oldItems,inputList]
     })
+
+      leftbar("leftbar")
+    
+    
+  }
+
+  const deleteItems=()=>{
+    alert("deleted")
   }
   return (
     <>
       <div className="main-div">
-          
+          <div className={bar}></div>
           <div className="container">
             <div className="todotxt">
               <h1>TO DO LIST</h1>
@@ -31,13 +40,23 @@ function App() {
               <ul>
               
               { Items.map((itemval)=>{
-                  return<li>{itemval}</li>
+                  return(
+                    <div className='fatherlist'>
+                      <div className="iconsW">
+                      <i className="fa-solid fa-circle-xmark"
+                      onClick={deleteItems}></i>
+                      </div>
+                     
+                      <li>{itemval}</li>
+                    </div>
+                  )
               })}
               
               </ul>
             </div>
              
           </div>
+        <div className={bar}></div>
       </div>
     </>
   );
